4db4ae71d15ec4073a45d8bfcc27f2f8f08c574a
wangmingdashi <359425883@qq.com>
2017-05-26 11:29:49 +0800

    1

---
+++ src/test/java/fffaa.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+/**
+ * DOCUMENT ME!
+ *
+ * @author   $author$
+ * @version  $Revision$, $Date$
+ */
+public class fffaa  {
+    //~ Instance fields --------------------------------------------------------------------------------------------------
+
+    @Test
+    public void dfdfdf() throws Exception {
+        System.out.println("wmmwmm");
+    }
+} // end class AccountManagerTest
